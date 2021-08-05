09447c5532f71ad85d87e40762cae9ca7dbe499b
Santiago <santiagojavierveiga@gmail.com>
2021-08-05 17:31:38 -0300

    fix(client/constants): modified Hexa colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,11 @@
 export default {
-  primary: '#202c41',
-  border: '#c6c6c6',
+  primary: '#ff4b6e',
+  secondary: '#ff9349',
+  border: '#ff4b6e',
   white: '#fff',
   gray: '#9ca5ab',
+  light_gray: '#cecece',
+  background_gradient: 'linear-gradient(180deg,#ff4b6e 0%, #ff9349 80%)',
   background: '#eef0f3',
   error: '#b55464',
 };
