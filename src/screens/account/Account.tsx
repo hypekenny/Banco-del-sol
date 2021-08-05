f4decc4a89cceca628cf669ad7fff0ee826c93c8
Santiago <santiagojavierveiga@gmail.com>
2021-08-05 20:18:43 -0300

    feat(client/Account): added fields and buttons

--- src/screens/account/Account.tsx
+++ src/screens/account/Account.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Text } from '../../components/Text';
+import { Button } from '../../components/Button';
 import { styles } from './AccountStyles';
+
+function asociateCVU() {}
+
+function shareCVU() {}
 
 export const Account = () => (
   <View style={styles.container}>
     <Text type="header">ACCOUNT</Text>
+    <Text>CVU</Text>
+    <Text>00000526789452134567</Text>
+    <Text>Nombre del Banco</Text>
+    <Text>Banco del Sol</Text>
+    <Text>Nombre del Titular</Text>
+    <Text>usuario loggeado</Text>
+    <Button type="outline" onPress={() => asociateCVU()}>
+      Asociar CVU
+    </Button>
+    <Button type="outline" onPress={() => shareCVU()}>
+      Compartir CVU
+    </Button>
   </View>
 );
