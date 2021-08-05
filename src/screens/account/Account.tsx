5993881a409c4a46d2818a59938ca8cfbd467b1e
Santiago <santiagojavierveiga@gmail.com>
2021-08-05 20:59:13 -0300

    fix(client/Account): modified Account View with info and buttons

--- src/screens/account/Account.tsx
+++ src/screens/account/Account.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from '../../components/Text';
-import { Button } from '../../components/Button';
+import { View, Text, TouchableHighlight } from 'react-native';
 import { styles } from './AccountStyles';
 
 function asociateCVU() {}
@@ -10,18 +8,25 @@ function shareCVU() {}
 
 export const Account = () => (
   <View style={styles.container}>
-    <Text type="header">ACCOUNT</Text>
-    <Text>CVU</Text>
-    <Text>00000526789452134567</Text>
-    <Text>Nombre del Banco</Text>
-    <Text>Banco del Sol</Text>
-    <Text>Nombre del Titular</Text>
-    <Text>usuario loggeado</Text>
-    <Button type="outline" onPress={() => asociateCVU()}>
-      Asociar CVU
-    </Button>
-    <Button type="outline" onPress={() => shareCVU()}>
-      Compartir CVU
-    </Button>
+    <View style={styles.container2}>
+      <View style={styles.cvu}>
+        <Text>CVU</Text>
+        <Text>00000526789452134567</Text>
+      </View>
+      <View style={styles.bankName}>
+        <Text>Nombre del Banco</Text>
+        <Text>Banco del Sol</Text>
+      </View>
+      <Text>Nombre del Titular</Text>
+      <Text>usuario loggeado</Text>
+    </View>
+    <View>
+      <TouchableHighlight onPress={() => asociateCVU()}>
+        <Text>Asociar CVU</Text>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => shareCVU()}>
+        <Text>Compartir CVU</Text>
+      </TouchableHighlight>
+    </View>
   </View>
 );
--- src/screens/account/AccountStyles.ts
+++ src/screens/account/AccountStyles.ts
@@ -4,50 +4,30 @@ import colors from '../../constants/colors';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.white,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  container2: {
+    padding: 30,
+    backgroundColor: colors.primary,
+  },
+  cvu: {
+    padding: 0,
+  },
+  bankName: {
+    padding: 0,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
     padding: 10,
   },
-  inputEmail: {
-    borderRadius: 50,
-    height: 45,
-    borderWidth: 2,
-    borderColor: '#FF6C5D',
-    paddingVertical: 0,
-
-    paddingLeft: 8,
-    paddingRight: 8,
-    marginTop: 10,
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
   },
-  inputPassword: {
-    borderRadius: 50,
-    height: 45,
-    borderWidth: 2,
-    borderColor: '#FF6C5D',
-    paddingVertical: 0,
-
-    paddingLeft: 8,
-    paddingRight: 8,
-    marginTop: -40,
-  },
-  inputRegister: {
-    borderRadius: 50,
-    height: 45,
-    borderWidth: 2,
-    borderColor: '#FF6C5D',
-    paddingVertical: 0,
-
-    paddingLeft: 8,
-    paddingRight: 8,
-    // marginTop: -40,
-  },
-  btn: {
-    backgroundColor: 'blue',
-  },
-  img: {
-    borderRadius: 50,
-    width: 80,
-    height: 80,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+  countText: {
+    color: '#FF00FF',
   },
 });
