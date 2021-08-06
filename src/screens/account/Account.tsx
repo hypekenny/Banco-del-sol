e31734b56462dbd36854f7ecb01a5accfa632fb6
Santiago <santiagojavierveiga@gmail.com>
2021-08-06 16:05:10 -0300

    fix(client/Account):  add template for future redux storage

--- src/screens/account/Account.tsx
+++ src/screens/account/Account.tsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, TouchableHighlight } from 'react-native';
 import { styles } from './AccountStyles';
 
-function asociateCVU() {}
+export function Account() {
+  useEffect(() => {
+    console.log('traigo de redux los datos del User');
+  }, []);
 
-function shareCVU() {}
+  function asociateCVU() {
+    alert('CVU asociado');
+  }
 
-export const Account = () => (
-  <View style={styles.container}>
-    <View style={styles.container2}>
-      <View style={styles.block}>
-        <Text>CVU</Text>
-        <Text>00000526789452134567</Text>
+  function shareCVU() {
+    alert('CVU copiado al portapapeles');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.container2}>
+        <View style={styles.block}>
+          <Text>CVU</Text>
+          <Text>00000526789452134567</Text>
+        </View>
+        <View style={styles.block}>
+          <Text>Nombre del Banco</Text>
+          <Text>Banco del Sol</Text>
+        </View>
+        <View style={styles.block}>
+          <Text>Nombre del Titular</Text>
+          <Text>usuario loggeado</Text>
+        </View>
       </View>
-      <View style={styles.block}>
-        <Text>Nombre del Banco</Text>
-        <Text>Banco del Sol</Text>
-      </View>
-      <View style={styles.block}>
-        <Text>Nombre del Titular</Text>
-        <Text>usuario loggeado</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableHighlight style={styles.button} onPress={() => asociateCVU()}>
+          <Text>Asociar CVU</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button} onPress={() => shareCVU()}>
+          <Text>Compartir CVU</Text>
+        </TouchableHighlight>
       </View>
     </View>
-    <View style={styles.buttonsContainer}>
-      <TouchableHighlight style={styles.button} onPress={() => asociateCVU()}>
-        <Text>Asociar CVU</Text>
-      </TouchableHighlight>
-      <TouchableHighlight style={styles.button} onPress={() => shareCVU()}>
-        <Text>Compartir CVU</Text>
-      </TouchableHighlight>
-    </View>
-  </View>
-);
+  );
+}
