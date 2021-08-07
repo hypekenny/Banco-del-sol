d21f8fc64d4c1b9fb01e379cdacbb71a420ba644
nicolas <cnico_97@hotmail.com>
2021-08-07 12:33:53 -0300

    WIP(login) playing with icons and gradient

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { View, Button, TouchableOpacity, Text, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useDispatch } from 'react-redux';
+import { MaterialIcons } from '@expo/vector-icons';
 import { login } from '../../redux/actions';
 import { styles } from './LoginStyles';
+
 // import { resFromBack } from '../../types/Types';
 
 export const Login = () => {
@@ -15,42 +18,49 @@ export const Login = () => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Email..."
-        placeholderTextColor="black"
-        value={user.email}
-        onChangeText={(text: string) => setUser({ ...user, email: text })}
-        keyboardType="email-address"
-        style={styles.inputEmail}
-      />
+      <LinearGradient colors={['#ff4b6e', '#ff9349']}>
+        <View>
+          <TextInput
+            placeholder="Email..."
+            placeholderTextColor="black"
+            value={user.email}
+            onChangeText={(text: string) => setUser({ ...user, email: text })}
+            keyboardType="email-address"
+            style={styles.inputEmail}
+          />
+          <TextInput
+            placeholder="Password..."
+            placeholderTextColor="black"
+            value={user.password}
+            onChangeText={(text: string) =>
+              setUser({ ...user, password: text })
+            }
+            secureTextEntry
+            style={styles.inputEmail}
+          />
 
-      <TextInput
-        placeholder="Password..."
-        placeholderTextColor="black"
-        value={user.password}
-        onChangeText={(text: string) => setUser({ ...user, password: text })}
-        secureTextEntry
-        style={styles.inputEmail}
-      />
+          <TouchableOpacity onPress={() => console.log('a')}>
+            <Text>Olvidaste tu mail?</Text>
 
-      <TouchableOpacity onPress={() => console.log('a')}>
-        <Text>Olvidaste tu mail?</Text>
-      </TouchableOpacity>
+            <MaterialIcons name="swap-horiz" size={32} color="white" />
+          </TouchableOpacity>
 
-      {user.password.length > 6 && user.email.length > 4 ? (
-        <Button
-          onPress={() => {
-            dispatch(login(user.email, user.password));
-          }}
-          title="Login"
-        />
-      ) : (
-        <Button
-          onPress={() => console.log('esto no puede pasar')}
-          title="Login"
-          disabled
-        />
-      )}
+          {user.password.length > 6 && user.email.length > 4 ? (
+            <Button
+              onPress={() => {
+                dispatch(login(user.email, user.password));
+              }}
+              title="Login"
+            />
+          ) : (
+            <Button
+              onPress={() => console.log('esto no puede pasar')}
+              title="Login"
+              disabled
+            />
+          )}
+        </View>
+      </LinearGradient>
     </View>
   );
 };
