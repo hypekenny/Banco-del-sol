1f468b6c148e4ca9f3401a0804389a4374a06c44
Santiago <santiagojavierveiga@gmail.com>
2021-08-07 13:30:00 -0300

    fix(client/Account): added buttons's constants styles

--- src/screens/account/Account.tsx
+++ src/screens/account/Account.tsx
@@ -1,6 +1,11 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect } from 'react';
 import { View, Text, TouchableHighlight } from 'react-native';
 import { styles } from './AccountStyles';
+import { ButtonPrimaryStyle } from '../../constants/ButtonPrymaryStyle';
+import { ButtonSecondaryStyle } from '../../constants/ButtonSecondaryStyle';
+import colors from '../../constants/colors';
 
 export function Account() {
   useEffect(() => {
@@ -17,26 +22,53 @@ export function Account() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.container2}>
-        <View style={styles.block}>
-          <Text>CVU</Text>
-          <Text>00000526789452134567</Text>
+      <LinearGradient
+        colors={[colors.primary, colors.secondary]}
+        style={styles.container2}
+      >
+        <View>
+          <View style={styles.block}>
+            <Text>CVU</Text>
+            <Text>00000526789452134567</Text>
+          </View>
+          <View style={styles.block}>
+            <Text>Nombre del Banco</Text>
+            <Text>Banco del Sol</Text>
+          </View>
+          <View style={styles.block}>
+            <Text>Nombre del Titular</Text>
+            <Text>usuario loggeado</Text>
+          </View>
         </View>
-        <View style={styles.block}>
-          <Text>Nombre del Banco</Text>
-          <Text>Banco del Sol</Text>
-        </View>
-        <View style={styles.block}>
-          <Text>Nombre del Titular</Text>
-          <Text>usuario loggeado</Text>
-        </View>
-      </View>
+      </LinearGradient>
       <View style={styles.buttonsContainer}>
-        <TouchableHighlight style={styles.button} onPress={() => asociateCVU()}>
-          <Text>Asociar CVU</Text>
+        <TouchableHighlight
+          style={ButtonPrimaryStyle.button}
+          onPress={() => asociateCVU()}
+        >
+          <View style={styles.ImageAndButton}>
+            <MaterialIcons
+              name="sync"
+              size={32}
+              color={colors.primary}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            />
+            <Text style={ButtonPrimaryStyle.text}>Asociar CVU</Text>
+          </View>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={() => shareCVU()}>
-          <Text>Compartir CVU</Text>
+        <TouchableHighlight
+          style={ButtonSecondaryStyle.button}
+          onPress={() => shareCVU()}
+        >
+          <View style={styles.ImageAndButton}>
+            <MaterialIcons
+              name="share"
+              size={32}
+              color="white"
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            />
+            <Text style={ButtonSecondaryStyle.text}>Compartir CVU</Text>
+          </View>
         </TouchableHighlight>
       </View>
     </View>
