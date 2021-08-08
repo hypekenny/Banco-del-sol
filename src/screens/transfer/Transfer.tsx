9d4979afe3b30d8d3327845caefe95defad03cbb
nicolas <cnico_97@hotmail.com>
2021-08-08 12:33:41 -0300

    feat/creating transfer, adding more types

--- src/screens/transfer/Transfer.tsx
+++ src/screens/transfer/Transfer.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text } from '../../components/Text';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { useSelector } from 'react-redux';
 import { styles } from './TransferStyles';
+import { RootState, Props } from '../../types/Types';
 
-export const Transfer = () => (
-  <View style={styles.container}>
-    <Text type="header">TRANSFER</Text>
-  </View>
-);
+export const Transfer = ({ navigation }: Props) => {
+  const accountStore = useSelector((state: RootState) => state.account);
+  const [data, setData] = useState({
+    email: '',
+    amount: 0,
+  });
+  return (
+    <View style={styles.container}>
+      <Text>TRANSFER</Text>
+      <TextInput
+        placeholder="Ingresá email"
+        placeholderTextColor="black"
+        value={data.email}
+        onChangeText={(text: string) => setData({ ...data, email: text })}
+        keyboardType="email-address"
+        style={styles.inputEmail}
+      />
+      <Text>{accountStore.balance}</Text>
+      <TextInput
+        placeholder="Ingresá email"
+        placeholderTextColor="black"
+        value={data.email}
+        onChangeText={(text: string) => setData({ ...data, email: text })}
+        keyboardType="email-address"
+        style={styles.inputEmail}
+      />
+      <TouchableOpacity onPress={() => console.log('pending')}>
+        enviar
+      </TouchableOpacity>
+    </View>
+  );
+};
