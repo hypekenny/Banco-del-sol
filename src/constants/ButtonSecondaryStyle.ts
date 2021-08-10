f1559c877e43df88cb58de8225ad775d905a2276
Ezequieldecu <ezequieldecu@gmail.com>
2021-08-10 09:49:38 -0300

    feat(add disable button for Register)

--- src/constants/ButtonSecondaryStyle.ts
+++ src/constants/ButtonSecondaryStyle.ts
@@ -22,6 +22,26 @@ export const ButtonSecondaryStyle = StyleSheet.create({
 
     elevation: 14,
   },
+  buttonDisable: {
+    width: 250,
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: 'white',
+    backgroundColor: 'grey',
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 26,
+
+    elevation: 14,
+  },
   text: {
     color: 'white',
     paddingLeft: 5,
