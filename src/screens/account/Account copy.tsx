144660ef8ad4f39c9ee93fa6e3f1b3ee01dcd317
Santiago <santiagojavierveiga@gmail.com>
2021-08-11 14:57:52 -0300

    fix(client/Account): added Loading animation

---
+++ src/screens/account/Account copy.tsx
@@ -0,0 +1,88 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { styles } from './AccountStyles';
+import { resFromBack } from '../../types/Types';
+import { ButtonPrimaryStyle } from '../../constants/ButtonPrymaryStyle';
+import colors from '../../constants/colors';
+import { fetchCvu } from '../../redux/actions';
+import Loading from '../loading/Loading';
+
+interface store {
+  cvuAsociate: boolean;
+}
+
+export function Account() {
+  const dispatch = useDispatch();
+  const cvuAsociate = useSelector((state: store) => state.cvuAsociate);
+  const accountDetails = useSelector((state: resFromBack) => state.account);
+  const userDetails = useSelector((state: resFromBack) => state.user);
+
+  const [associatePushed, setAssociatePushed] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  function asociateCVU() {
+    /* Simulacion de demora de pedido a la api */
+    function resolveAfter3Seconds() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve('resolved');
+        }, 3000);
+      });
+    }
+
+    async function asyncCall() {
+      await resolveAfter3Seconds();
+      dispatch(fetchCvu());
+      setAssociatePushed(true)
+      setLoaded(true);
+      alert('CVU asociado');
+    }
+    asyncCall();
+  }
+
+  function shareCVU() {
+    alert('CVU copiado al portapapeles');
+  }
+
+  return (
+    <View style={styles.container}>
+      {
+        !loaded && !associatePushed ? (
+        <View>
+          <TouchableOpacity
+            style={ButtonPrimaryStyle.button}
+            onPress={() => asociateCVU()}
+          >
+            <View style={styles.ImageAndButton}>
+              <MaterialIcons
+                name="sync"
+                size={32}
+                color={colors.primary}
+                style={{ paddingLeft: 5, paddingRight: 5 }}
+              />
+              <Text style={ButtonPrimaryStyle.text}>Asociar CVU</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={ButtonPrimaryStyle.button}
+            onPress={() => shareCVU()}
+          >
+            <View style={styles.ImageAndButton}>
+              <MaterialIcons
+                name="share"
+                size={32}
+                color={colors.primary}
+                style={{ paddingLeft: 5, paddingRight: 5 }}
+              />
+              <Text style={ButtonPrimaryStyle.text}>Compartir CVU</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )
+      }
+    </View>        
+  );
+}
---
+++ src/screens/loading/LoadingStyles.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
