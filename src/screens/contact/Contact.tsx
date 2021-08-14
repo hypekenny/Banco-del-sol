74f5c9a490d3df9ae6dff993c9a9283959414ac9
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2021-08-14 02:27:11 -0300

    Coneccion con redux.

--- src/screens/contact/Contact.tsx
+++ src/screens/contact/Contact.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 // import { styles } from './ContactStyles';
+import { useSelector } from 'react-redux';
 import { loginStackParamList } from '../../types/Types';
 
 type Props = {
@@ -9,12 +10,25 @@ type Props = {
 };
 
 export const Contact = ({ navigation }: Props) => {
+  const contact = useSelector(state => state.Contacts);
+  console.log('Contact', contact);
+
   return (
     <View>
       <Button
         title="Press button"
         onPress={() => navigation.push('ContactAdd')}
       />
+      {console.log('aasdsadasdas', contact)}
+
+      {contact.map((contacto, i) => {
+        return contacto.name ? (
+          <View key={i}>
+            <Text>{contacto.email}</Text>
+            <Text>{contacto.name}</Text>
+          </View>
+        ) : null;
+      })}
     </View>
   );
 };
--- src/screens/contact/ContactAdd.tsx
+++ src/screens/contact/ContactAdd.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import { View, TextInput } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { getEmail } from '../../redux/actions';
 import { styles } from './ContactStyles';
+import { loginStackParamList } from '../../types/Types';
 
-export const ContactAdd = () => {
-  const [text, onChangeText] = React.useState('');
+type Props = {
+  navigation: StackNavigationProp<loginStackParamList, 'List'>;
+};
+
+export const ContactAdd = ({ navigation }: Props) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = React.useState('');
+  const [name, onChangeText] = React.useState('');
+  const idToken = useSelector(state => state.token);
+  const contact = useSelector(state => state.Contacts);
+
+  useEffect(() => {
+    console.log(contact);
+  }, [contact]);
 
   return (
     <View style={styles.containerAdd}>
       <TextInput
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={setEmail}
+        value={email}
         placeholder="Email de tu contacto"
       />
+
       <TextInput
         onChangeText={onChangeText}
-        value={text}
+        value={name}
         placeholder="Nombre de tu contacto"
       />
+
+      <TouchableOpacity
+        onPress={() => dispatch(getEmail(email, name, idToken))}
+      >
+        <Text>INGRESAR</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.push('Contact')}>
+        <Text>ta</Text>
+      </TouchableOpacity>
     </View>
   );
 };
