fdb7612e54f1af84ac033a55bde476430d1b8c14
Santiago <smferro@gmail.com>
2021-08-17 20:54:30 -0300

    credentials deleted

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -13,8 +13,8 @@ export const Login = ({ navigation }: Props) => {
   const dispatch = useDispatch();
 
   const [user, setUser] = useState({
-    email: 'ferro@hotmail.com',
-    password: '123QWE&',
+    email: '',
+    password: '',
     amount: 0,
   });
 
