4ce923556fc8fce958cd225a396bf33e4bc62237
Santiago <smferro@gmail.com>
2021-08-18 15:20:02 -0300

    changes in login

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import { View, TouchableOpacity, Text, TextInput } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { LinearGradient } from 'expo-linear-gradient';
-import { login, setLoadingTrue } from '../../redux/actions';
+import {
+  cleanErrors,
+  login,
+  setLoadingFalse,
+  setLoadingTrue,
+} from '../../redux/actions';
 import { styles } from './LoginStyles';
 import { Props, resFromBack, RootState } from '../../types/Types';
 import { ButtonPrimaryStyle } from '../../constants/ButtonPrymaryStyle';
@@ -12,17 +17,30 @@ import { LoadingFull } from '../loading2/LoadingFull';
 export const Login = ({ navigation }: Props) => {
   const userStore = useSelector((state: resFromBack) => state.user);
   const loading = useSelector((state: RootState) => state.loading);
+  const error = useSelector((state: RootState) => state.errors);
   const dispatch = useDispatch();
 
   const [user, setUser] = useState({
-    email: '',
-    password: '',
+    email: 'ferro@hotmail.com',
+    password: '123QWE&',
     amount: 0,
   });
+
+  if (error.length) {
+    dispatch(setLoadingFalse());
+    setTimeout(() => {
+      dispatch(cleanErrors());
+    }, 3000);
+  }
 
   return (
     <View style={styles.container}>
       <LoadingFull show={loading} />
+      {error.length ? (
+        <View style={styles.msgText}>
+          <Text style={styles.TextAdd}>{error}</Text>
+        </View>
+      ) : null}
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Email...."
