d94367e7bb610a4010d68ea31ebadd8a83295dd9
Santiago <smferro@gmail.com>
2021-08-20 01:13:54 -0300

    forgot pass styles

--- src/screens/forgotPassword/ForgotPasswordStyles.ts
+++ src/screens/forgotPassword/ForgotPasswordStyles.ts
@@ -103,4 +103,42 @@ export const styles = StyleSheet.create({
     borderRadius: 999,
     transform: [{ scaleX: 3 }],
   },
+  header: {
+    height: 75,
+    width: '100%',
+  },
+
+  title: {
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 70,
+    top: 0,
+    marginTop: '-0.3%',
+    width: '30%',
+  },
+
+  textTitle: {
+    position: 'absolute',
+    left: 5,
+    top: 15,
+    fontSize: 20,
+    color: 'white',
+    fontWeight: '500',
+    width: '180%',
+  },
+  back: {
+    position: 'absolute',
+    height: 70,
+    width: 70,
+    left: 0,
+    top: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  icon: { marginTop: '-18%' },
+
+  headerOne: { backgroundColor: '#000', height: 63 },
 });
