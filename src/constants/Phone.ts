9e4bba4e301c0cf1ba642edece7f640b53f48ff4
nicolas <cnico_97@hotmail.com>
2021-08-25 12:56:50 -0300

    creating phone style on constants for everyone to use

---
+++ src/constants/Phone.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const Phone = StyleSheet.create({
+  phone: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'space-around',
+    backgroundColor: 'red',
+  },
+  filler: {
+    height: '10%',
+  },
+});
