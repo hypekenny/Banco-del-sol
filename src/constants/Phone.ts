9a21beaec69b6723a098c471d0359a6b0181f631
nicolas <cnico_97@hotmail.com>
2021-08-25 12:59:11 -0300

    fix red to white on phone background

--- src/constants/Phone.ts
+++ src/constants/Phone.ts
@@ -1,11 +1,12 @@
 import { StyleSheet } from 'react-native';
+import { colors } from 'react-native-elements';
 
 export const Phone = StyleSheet.create({
   phone: {
     height: '100%',
     width: '100%',
     justifyContent: 'space-around',
-    backgroundColor: 'red',
+    backgroundColor: colors.white,
   },
   filler: {
     height: '10%',
