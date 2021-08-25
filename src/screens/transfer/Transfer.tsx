f8f0a78f50decddca87d16266eb70a8f772e38c2
nicolas <cnico_97@hotmail.com>
2021-08-25 19:19:03 -0300

    adding phone image on transfer

--- src/screens/transfer/Transfer.tsx
+++ src/screens/transfer/Transfer.tsx
@@ -34,6 +34,16 @@ export const Transfer = ({ navigation }: Props) => {
 
   return (
     <View style={StylesCon.phone}>
+      <img
+        style={{
+          width: 411,
+          height: 813,
+          position: 'absolute',
+          alignSelf: 'center',
+        }}
+        src="https://cdn.discordapp.com/attachments/872492726397042688/880174589605478400/Mockup_-_android_-_BDS_app.png"
+        alt=""
+      />
       <View style={StylesCon.filler} />
       <View style={styles.container}>
         <LoadingFull show={loading} />
